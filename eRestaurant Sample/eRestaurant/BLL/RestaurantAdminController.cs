047d121de0a4a89a881ce1f7d88126aac10a1e15
pjhas2 <pjhas2@studentmail.nait.ca>
2014-09-17 14:16:49 -0600

    Added query coding

--- eRestaurant Sample/eRestaurant/BLL/RestaurantAdminController.cs
+++ eRestaurant Sample/eRestaurant/BLL/RestaurantAdminController.cs
@@ -53,11 +53,19 @@ namespace eRestaurant.BLL
         #region Query
         public List<Waiter> ListAllWaiters()
         {
-            throw new NotImplementedException();
+            using (RestaurantContext context = new RestaurantContext())
+            {
+
+                return context.Waiters.ToList();
+            }
         }
         public Waiter GetWaiter(int waiterId)
         {
-            throw new NotImplementedException();
+            using (RestaurantContext context = new RestaurantContext())
+            {
+
+                return context.Waiters.Find(waiterId);
+            }
         }
         #endregion
         #endregion
