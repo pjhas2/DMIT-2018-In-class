7674c075fef66dce3c875b3e28bcc74794b10e40
pjhas2 <pjhas2@studentmail.nait.ca>
2014-10-21 13:48:29 -0600

    Generated property stub

--- eRestaurant Sample/eRestaurant/Entities/POCOs/CategoryMenuItem.cs
+++ eRestaurant Sample/eRestaurant/Entities/POCOs/CategoryMenuItem.cs
@@ -9,5 +9,14 @@ namespace eRestaurant.Entities.POCOs
     public class CategoryMenuItem
     {
 
+        public string CategoryDescription { get; set; }
+
+        public string ItemDescription { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Calories { get; set; }
+
+        public string Comment { get; set; }
     }
 }
